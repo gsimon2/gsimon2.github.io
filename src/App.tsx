afa44a533b6a858cb57dbacbe6c545064b6b892a
Glen Simon <glen.a.simon@gmail.com>
2021-06-11 10:53:12 -0400

    Set up required packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Header from './Header';
+
+const PageWrapper = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  background-color: #282c34;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const AppContentWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      <AppContentWrapper>
+        <Header />
+         <span>Hello</span>
+      </AppContentWrapper>
+    </PageWrapper>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className="App-header">
+            <a href="/Resume" target="_black"> test</a>
+        </header>
+    );
+};
+
+export default Header;

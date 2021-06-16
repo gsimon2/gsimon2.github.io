1832a66c5bbb360b40f86fc9aae91d0680a1a893
Glen Simon <glen.a.simon@gmail.com>
2021-06-16 14:04:03 -0400

    Update alert banner

--- src/components/AlertBanner.tsx
+++ src/components/AlertBanner.tsx
@@ -1,22 +1,31 @@
+import { makeStyles, Theme, useTheme } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import React, { useState } from 'react';
-import styled from 'styled-components';
+
 import CssConstants from '../constants/CssConstants';
 
-const AlertWrapper = styled.div`
-    max-width: ${CssConstants.appContentMaxWidth};
-    margin: 1rem auto 0;
-`;
+const useStyles = makeStyles((theme: Theme) => ({
+    alert: {
+        border: `1px solid ${CssConstants.themes.shared.accentColor}`,
+        boxShadow: theme.shadows[1]
+    },
+    wrapper: {
+        maxWidth: CssConstants.appContentMaxWidth,
+        margin: '1rem auto 0'
+    }
+}));
 
 const AlertBanner: React.FC = () => {
     const [displayAlert, setDisplayAlert] = useState(true);
+    const theme = useTheme<Theme>();
+    const classes = useStyles(theme);
 
     return (
         <>
         { displayAlert ?
-            <AlertWrapper>
-                <Alert severity="info" onClose={() => setDisplayAlert(false)}>Site is under development - More to come soon! = D</Alert>
-            </AlertWrapper> :
+            <div className={classes.wrapper}>
+                <Alert severity="info" className={classes.alert} onClose={() => setDisplayAlert(false)}>Site is under development - More to come soon! = D</Alert>
+            </div> :
             null
         }
         </>

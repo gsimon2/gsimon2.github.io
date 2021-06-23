1ccbc55ee5d1f9f866e6aab9609e0d4ac86f7a46
Glen Simon <glen.a.simon@gmail.com>
2021-06-23 13:44:30 -0400

    Update home page to useStyles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,58 +1,61 @@
-import { Grow, useMediaQuery } from '@material-ui/core';
+import { Grow, makeStyles, Theme, useMediaQuery, useTheme } from '@material-ui/core';
 import React from 'react';
-import styled from 'styled-components';
 import profileImag from '../assets/glen-mt.jpg';
 import CssConstants from '../constants/CssConstants';
 import {PageWrapper} from '../util/sharedElements';
 
-const Img = styled.img`
-    border-radius: 20%;
-    object-fit: contain;
-    justify-self: center;
-`;
-
-const MobileImg = styled(Img)`
-    min-width: 0;
-    max-width: 22rem;
-    max-height: 100%;
-`;
-
-const DesktopImg = styled(Img)`
-    max-width: 100%;
-    max-height: 35rem;
-`;
-
-const GridContainerColumns = styled.div`
-    display: grid;
-    grid-template-columns: minmax(5rem, 1fr) 1fr;
-`;
-
-const GridContainerRows = styled.div`
-    display: grid;
-    grid-template-rows: minmax(5rem, 1fr) 1fr;
-`;
-
-const TextContainer = styled.span<{isMobileView: boolean}>`
-    margin: ${props => props.isMobileView ? '2rem auto' : '4rem 1rem 0 4rem'};
-    word-break: break-word;
-`;
+const useStyles = makeStyles((theme: Theme) => ({
+    img: {
+        borderRadius: '20%',
+        objectFit: 'contain',
+        justifySelf: 'center',
+        minWidth: 0,
+        boxShadow: theme.shadows[5],
+        border: `1px solid ${CssConstants.themes.shared.accentColor}`
+    },
+    imgMobile: {
+        maxHeight: '100%',
+        maxWidth: 'min(25rem, 100%)'
+    },
+    imgDesktop: {
+        maxHeight: '35rem',
+        maxWidth: '100%'
+    },
+    gridContainer: {
+        display: 'grid',
+    },
+    gridMobile: {
+        gridTemplateRows: 'minmax(5rem, 1fr) 1fr'
+    },
+    gridDesktop: {
+        gridTemplateColumns: 'minmax(5rem, 1fr) 1fr'
+    },
+    textContainerMobile: {
+        wordBreak: 'break-word',
+        margin: '2rem auto'
+    },
+    textContainerDesktop: {
+        wordBreak: 'break-word',
+        margin: '4rem 1rem 0 4rem'
+    }
+}));
 
 const Home: React.FC = () => {
     const isMobileView = useMediaQuery(`(max-width:${CssConstants.mobileBreakPoint})`);
-    const GridContainer = isMobileView ? GridContainerRows : GridContainerColumns;
-    const ImageElemnt = isMobileView ? MobileImg : DesktopImg ;
+    const theme = useTheme<Theme>();
+    const classes = useStyles(theme);
 
     return (
         <>
             <PageWrapper>
-                <GridContainer>
+                <div className={`${classes.gridContainer} ${isMobileView ? classes.gridMobile : classes.gridDesktop}`}>
                     <Grow in={true}>
-                        <ImageElemnt src={profileImag} />
+                        <img src={profileImag} className={`${classes.img} ${isMobileView ? classes.imgMobile : classes.imgDesktop}`} alt="profile" />
                     </Grow>
                     <Grow in={true}>
-                        <TextContainer isMobileView={isMobileView}>Hi I'm Glen! Eventually I will talk about myself here</TextContainer>
+                        <span className={isMobileView ? classes.textContainerMobile : classes.textContainerDesktop}>Hi I'm Glen! Eventually I will talk about myself here</span>
                     </Grow>
-                </GridContainer>
+                </div>
             </PageWrapper>
         </>
     );

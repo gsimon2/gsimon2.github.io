dededef940533d565d598482893612dfa8d1ed65
Glen Simon <glen.a.simon@gmail.com>
2021-10-27 08:22:37 -0400

    Add retry logic for resume gview

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,5 +1,5 @@
 import { CircularProgress } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Constants from '../constants/Constants';
 import CssConstants from '../constants/CssConstants';
@@ -17,13 +17,29 @@ const ContentWrapper = styled.div`
     align-items: center;
 `;
 
+let timeoutId: NodeJS.Timeout;
+const timeoutDurationInMs = 1500;
+
 const Resume: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [loadAttempts, setLoadAttempts] = useState(0);
+
+    useEffect(() => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            setLoadAttempts(loadAttempts + 1)
+        }, timeoutDurationInMs);
+    }, [loadAttempts, setLoadAttempts]);
+
+    const onLoad = () => {
+        clearTimeout(timeoutId);
+        setIsLoading(false)
+    };
 
     return (
         <>
             {isLoading && <ContentWrapper><CircularProgress /></ContentWrapper>}
-            <ResumeIFrame title="ResumeWrapper" src={Constants.resumeIframeSource} onLoad={() => setIsLoading(false)} />
+            <ResumeIFrame key={loadAttempts} title="ResumeWrapper" src={Constants.resumeIframeSource} onLoad={onLoad} />
         </>
     );
 };
